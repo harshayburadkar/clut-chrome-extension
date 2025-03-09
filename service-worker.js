e1394e4a172a5259785112d1dc137ea534d39af0
Harshay Buradkar <harshay.imag3@gmail.com>
2025-03-09 23:36:35 +0530

    remove GA for now

--- service-worker.js
+++ service-worker.js
@@ -330,7 +330,7 @@ var quickSwitchActiveUsage = function() {
 		} else if(quickActive < 5){
 			quickActive++;
 		} else if(quickActive >= 5) {
-			_gaq.push(['_trackEvent', 'activeUsage', 'quick']);
+			// _gaq.push(['_trackEvent', 'activeUsage', 'quick']);
 			quickActive = -1;
 		}
 	}
@@ -344,27 +344,28 @@ var slowSwitchActiveUsage = function() {
 		} else if(slowActive < 5){
 			slowActive++;
 		} else if(slowActive >= 5) {
-			_gaq.push(['_trackEvent', 'activeUsage', 'slow']);
+			// _gaq.push(['_trackEvent', 'activeUsage', 'slow']);
 			slowActive = -1;
 		}
 	}
 }
 
+//TODO disable for now, need to find a way to migrate later
 //check for actual active users
-var _AnalyticsCode = 'UA-51920707-1';
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', _AnalyticsCode]);
-_gaq.push(['_trackPageview']);
+// var _AnalyticsCode = 'UA-51920707-1';
+// var _gaq = _gaq || [];
+// _gaq.push(['_setAccount', _AnalyticsCode]);
+// _gaq.push(['_trackPageview']);
 
-(function() {
-	var ga = document.createElement('script');
-	ga.type = 'text/javascript';
-	ga.async = true;
-	ga.src = 'https://ssl.google-analytics.com/ga.js';
-	//ga.src = 'https://ssl.google-analytics.com/u/ga_debug.js';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(ga, s);
-})();
+// (function() {
+// 	var ga = document.createElement('script');
+// 	ga.type = 'text/javascript';
+// 	ga.async = true;
+// 	ga.src = 'https://ssl.google-analytics.com/ga.js';
+// 	//ga.src = 'https://ssl.google-analytics.com/u/ga_debug.js';
+// 	var s = document.getElementsByTagName('script')[0];
+// 	s.parentNode.insertBefore(ga, s);
+// })();
 document.addEventListener('DOMContentLoaded', function () {
 	domLoaded = true;
 
